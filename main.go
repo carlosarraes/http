7745950a20eb5e3f0731517a6764ffb31e033173
carraes <carraeshb@gmail.com>
2023-01-03 15:21:09 -0300

    feat: read body

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 )
+
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("http://google.com")
@@ -11,5 +14,13 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println(resp)
+	lw := logWriter{}
+
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+	return len(bs), nil
 }
